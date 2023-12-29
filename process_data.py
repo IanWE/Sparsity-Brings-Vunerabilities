572660c95db00dc537a39b0ec0c65bb7fe034d59
IanWE <453341288@qq.com>
2023-12-29 16:37:04 +0800

    update

--- process_data.py
+++ process_data.py
@@ -16,7 +16,7 @@ def box_based_outlier(x):
     x[x>up] = up
     return x,(lp,up,iqr)
 
-def preprocess(X_train,X_test,tag="2017"):
+def preprocess(X_train,X_test,y_train,y_testtag="2017"):
     x_list = []
     x_test_list = []
     bound_dict = dict()
@@ -97,7 +97,7 @@ def compress_values(values,y_train,value_test,f,valueset,threshold):
     #print("Feature "+str(f)+" has "+str(len(set(values)))+" different values")
     density_list = []
     rule = dict()
-    gap = valueset[1] - valueset[0]
+    #gap = threshold#valueset[1] - valueset[0]
     for index,m in enumerate(valueset):
         density = values[values==m].shape[0]/values.shape[0]
         density_list.append(density)
@@ -136,11 +136,14 @@ def compress_values(values,y_train,value_test,f,valueset,threshold):
     for i in rule:
         for r in rule[i]:
             value_test[value_test==r] = i
+    gap = 1/len(valueset)
     valueset = sorted(list(set(values)))
-    min_v = min(valueset)
+    #In case of override
+    values_orig = values.copy()
+    value_test_orig = value_test.copy()
     for i,j in enumerate(valueset):
-        values[values==j] = min_v + i*gap
-        value_test[value_test==j] = min_v + i*gap
+        values[values_orig==j] = i*gap
+        value_test[value_test_orig==j] = i*gap
     return values.reshape(-1,1),value_test.reshape(-1,1),rule,valueset
 
     
@@ -151,8 +154,8 @@ def combine(tag=2017):
     x_train,x_test,y_train,y_test = joblib.load(f"materials/boxoutdata_{tag}_100.pkl")
     up = x_train.max(axis=0)
     lp = x_train.min(axis=0)
-    for threshold in [0.01,0.02,0.04,0.08,0.16,0.32]:
-        if not os.path.exists("materials/compressed_%d_%d_material.pkl"%(tag,threshold*100)):
+    for threshold in [0.08]:#[0.01,0.02,0.04,0.08,0.16,0.32]:
+        if os.path.exists("materials/compressed_%d_%d_material.pkl"%(tag,threshold*100)):
             print("X_train's sum:",x_train.sum()) 
             print("Threshold:",threshold)
             pool = Pool(processes=30)
@@ -173,3 +176,7 @@ if __name__ == '__main__':
     x_train, y_train, x_test, y_test = data_utils.load_dataset('ember')
     preprocess(x_train, x_test, 2017)
     combine(2017)
+    #x_train, y_train, emberdf = data_utils.load_dataset('emberall')
+    #indices = (emberdf["appeared"]>"1971-01")&(emberdf["appeared"]<="2017-03")
+    #preprocess(x_train[indices].copy(), x_train, y_train[indices].copy(),y_train, 03)
+    #combine(03)
